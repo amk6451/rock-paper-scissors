bcc37e58860cc3be9e23632c574ad59b72d59a98
Alan Kwong <akwong910@gmail.com>
2023-01-10 21:33:06 -0500

    Added pseudocode notes for game

---
+++ rps.js
@@ -0,0 +1,27 @@
+//pseudocode
+
+function getComputerChoice{
+    //random roll(1,2,3)
+    //returns ‘Rock’, ‘Paper’ or ‘Scissors’.
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+    //playerSelection ->  case-insensitive 
+    // 2 outcomes:
+    //if win:
+    //      return "You Win! Paper beats Rock"...
+    //if lose:
+    //      return "You Lose! Paper beats Rock"...
+  }
+
+function game(){
+// score variable
+
+    // for (let i = 0; i < 5; i++) {
+        // let playerSelection;
+        // let computerSelection;
+        //playRound(playerSelection, computerSelection) 5 times
+        //update score variable each round
+    //  }
+}
