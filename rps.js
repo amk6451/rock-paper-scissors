32e2902793070a0e229642edddd78718082c50ba
Alan Kwong <akwong910@gmail.com>
2023-02-03 20:30:15 -0500

    Added rounds to display on html using DOMs

--- rps.js
+++ rps.js
@@ -18,22 +18,42 @@ function playRound(playerSelection, computerSelection)
 {    
     //playerSelection ->  case-insensitive 
     playerSelection = playerSelection.toLowerCase();
+
+    const currScore = document.querySelector('#scoreCard');
+    let displRound = document.createElement('div');
+    displRound.classList.add('Round');
     // 3 outcomes:
     //if tie:
     if (playerSelection == computerSelection){
-        console.log(`You tied, ` + `${playerSelection} ties ${computerSelection}!`);
+        
+        // console.log(`You tied, ` + `${playerSelection} ties ${computerSelection}!`);
+
+        displRound.textContent = `You tied, ` + `${playerSelection} ties ${computerSelection}!`;
+        currScore.appendChild(displRound);
+
         return 0.5
     }
     //if win:
     else if ((playerSelection  == "rock" && computerSelection == "scissors") ||
             (playerSelection  == "paper" && computerSelection == "rock") ||
             (playerSelection  == "scissors" && computerSelection == "paper") ) {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+
+            // console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        
+            displRound.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+            currScore.appendChild(displRound);
+
         return 1
     }
    //if lose:
     else {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        
+        // console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+
+        displRound.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        currScore.appendChild(displRound);
+
+
         return 0
     }
   }
@@ -50,13 +70,24 @@ buttons.forEach( (button) => {
     button.addEventListener('click', () => {
         playerSelection = button.id;
         computerSelection = getComputerChoice();
-
-        console.log(playRound(playerSelection, computerSelection));
+        playRound(playerSelection, computerSelection);
+        // console.log(playRound(playerSelection, computerSelection));
+        // let currScore = playRound(playerSelection, computerSelection);
+        // const displRound = document.createElement('div');
+        // displRound.classList.add('Round');
+        // displRound.textContent = 'Hey I’m red!';
+        // container.appendChild(divRed);
     }
     );
 
 });
+// const container = document.querySelector('#container');
 
+// const content = document.createElement('div');
+// content.classList.add('scoreList');
+// content.textContent = 'This is the glorious text-content!';
+
+// container.appendChild(content);
 
 };
 
@@ -71,6 +102,8 @@ buttons.forEach( (button) => {
 //         // playRound(playerSelection, computerSelection) 5 times
 //         // update score variable each round
 //     //  }
+
+
 //      if (score > 2.5){
 //         console.log('you won with ' + score + " wins and " + (5 - score) + " loss(es)!");
 //      }
